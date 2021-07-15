cd84d1f4f24b1fb7404d070d2e4ada2bcdb70643
Павлов Иван Иванович <w3dipmail@gmail.com>
2021-07-15 23:13:57 +0300

    init commit

---
+++ task1/task1.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import unittest
+
+def parse(path_to_file):
+    # Поместите ваш код здесь.
+    # ВАЖНО!!!
+    # При открытии файла, добавьте в функцию open необязательный параметр
+    # encoding='utf-8', его отсутствие в коде будет вызвать падение вашего
+    # решения на грейдере с ошибкой UnicodeDecodeError
+    return [imgs, headers, linkslen, lists]
+
+
+class TestParse(unittest.TestCase):
+    def test_parse(self):
+        test_cases = (
+            ('wiki/Stone_Age', [13, 10, 12, 40]),
+            ('wiki/Brain', [19, 5, 25, 11]),
+            ('wiki/Artificial_intelligence', [8, 19, 13, 198]),
+            ('wiki/Python_(programming_language)', [2, 5, 17, 41]),
+            ('wiki/Spectrogram', [1, 2, 4, 7]),)
+
+        for path, expected in test_cases:
+            with self.subTest(path=path, expected=expected):
+                self.assertEqual(parse(path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
